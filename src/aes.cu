04beb86ad4576e898c59f232c9b60c3dcd476693
Felipe Del Campo <delcampofelipe@yahoo.com.br>
2012-10-25 23:50:53 -0200

    Ainda com problemas na decifragem do serial

--- src/aes.cu
+++ src/aes.cu
@@ -17,7 +17,7 @@ __global__ void CSubBytes(uint8_t *estado) {
 
 void SubBytes(uint8_t *estado, ulong offset) {
 	for(register int i=0; i<16; i++){
-		estado[i*offset] = Sbox[estado[i*offset]];
+		estado[i+(15*offset)] = Sbox[estado[i+(15*offset)]];
 	}
 }
 
@@ -27,12 +27,12 @@ __global__ void CInvSubBytes(uint8_t *estado) {
 
 void InvSubBytes(uint8_t *estado, ulong offset) {
 	for(register int i=0; i<16; i++){
-		estado[i*offset] = InvSbox[estado[i*offset]];
+		estado[i+(15*offset)] = InvSbox[estado[i+(15*offset)]];
 	}
 }
 
 __global__ void CShiftRows(uint8_t *estado) {
-	unsigned int row  = threadIdx.x;
+	uint64_t row  = threadIdx.x;
 	uint8_t tmp[4];
 
 	tmp[0] = estado[row + 4*(0+row) % 16];
@@ -46,9 +46,9 @@ __global__ void CShiftRows(uint8_t *estado) {
 	estado[row + 4*3] = tmp[3];
 }
 
-void ShiftRows(uint8_t *estado, uint8_t offset) {
-	for(register int i=0; i<16; i++){
-		uint8_t row  = i*offset;
+void ShiftRows(uint8_t *estado, ulong offset) {
+	for(register int i=0; i<4; i++){
+		ulong row  = i+(15*offset);
 		uint8_t tmp[4];
 	
 		tmp[0] = estado[row + 4*(0+row) % 16];
@@ -64,7 +64,7 @@ void ShiftRows(uint8_t *estado, uint8_t offset) {
 }
 
 __global__ void CInvShiftRows(uint8_t *estado) {
-	unsigned int row  = threadIdx.x;
+	uint64_t row  = threadIdx.x;
 	uint8_t tmp[4];
 
 	tmp[0] = estado[row + 4*(0-row) % 16];
@@ -80,8 +80,8 @@ __global__ void CInvShiftRows(uint8_t *estado) {
 
 
 void InvShiftRows(uint8_t *estado, ulong offset) {
-	for(register int i=0; i<16; i++){
-		uint8_t row  = i*offset;
+	for(register int i=0; i<4; i++){
+		ulong row  = i+(15*offset);
 		uint8_t tmp[4];
 	
 		tmp[0] = estado[row + 4*(0-row) % 16];
@@ -96,7 +96,7 @@ void InvShiftRows(uint8_t *estado, ulong offset) {
 	}
 }
 __global__ void CMixColumns(uint8_t *estado) {
-	uint8_t base = threadIdx.x << 2;
+	ulong base = threadIdx.x << 2;
 	uint8_t t[4];
 	
 	t[0] = caes_mul(0x02, estado[base]) ^ caes_mul(0x03, estado[base+1]) ^ estado[base+2] ^ estado[base+3];
@@ -112,7 +112,7 @@ __global__ void CMixColumns(uint8_t *estado) {
 
 void MixColumns(uint8_t *estado, ulong offset) {
 	for(register int i=0; i<16; i++) {
-		uint8_t base = (i*offset) << 2;
+		ulong base = (i+(15*offset)) << 2;
 		uint8_t t[4];
 	
 		t[0] = aes_mul(0x02, estado[base]) ^ aes_mul(0x03, estado[base+1]) ^ estado[base+2] ^ estado[base+3];
@@ -144,7 +144,7 @@ __global__ void CInvMixColumns(uint8_t *estado) {
 
 void InvMixColumns(uint8_t *estado, ulong offset) {
 	for(uint8_t i=0; i<16; i++) {
-		uint8_t base = (i*offset) << 2;
+		uint8_t base = (i+(15*offset)) << 2;
 		uint8_t t[4];
 	
 		t[0] = aes_mul(0x0e, estado[base]) ^ aes_mul(0x0b, estado[base+1]) ^ aes_mul(0x0d, estado[base+2]) ^ aes_mul(0x09, estado[base+3]);
@@ -165,57 +165,57 @@ __global__ void CAddRoundKey(uint8_t *estado, uint8_t *chave) {
 
 void AddRoundKey(uint8_t *estado, uint8_t *chave, ulong offset) {
 	for(uint8_t i=0; i<16; i++) {
-		estado[i*offset] ^= chave[i];
+		estado[i+(15*offset)] ^= chave[i];
 	}
 }
 
 void cinvAes(uint8_t *cp, uint8_t *cW, uint8_t Nr, ulong n) {
 	
 	register uint8_t i;
-	register uint8_t j;
-	uint8_t tmp[16*n];
-	cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-	printf("0 str ");
-	for(j=0; j < 16*n; j++) {
-		printf("%02X", tmp[j]);
-	}
-	printf("\n");
+//	register uint8_t j;
+//	uint8_t tmp[16*n];
+//	cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//	printf("0 str ");
+//	for(j=0; j < 16*n; j++) {
+//		printf("%02X", tmp[j]);
+//	}
+//	printf("\n");
 	CAddRoundKey<<<1,16*n>>>(cp, cW+(Nr << 4));
-	cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-	printf("0 add ");
-	for(j=0; j < 16*n; j++) {
-		printf("%02X", tmp[j]);
-	}
-	printf("\n");
+//	cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//	printf("0 add ");
+//	for(j=0; j < 16*n; j++) {
+//		printf("%02X", tmp[j]);
+//	}
+//	printf("\n");
 	for(i=Nr; i>1; i--) {
 		CInvShiftRows<<<1,16*n>>>(cp);
-		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-		printf("%d shi ",i);
-		for(j=0; j < 16*n; j++) {
-			printf("%02X", tmp[j]);
-		}
-		printf("\n");
+//		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//		printf("%d shi ",i);
+//		for(j=0; j < 16*n; j++) {
+//			printf("%02X", tmp[j]);
+//		}
+//		printf("\n");
 		CInvSubBytes<<<1,16*n>>>(cp);
-		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-		printf("%d sub ",i);
-		for(j=0; j < 16*n; j++) {
-			printf("%02X", tmp[j]);
-		}
-		printf("\n");
+//		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//		printf("%d sub ",i);
+//		for(j=0; j < 16*n; j++) {
+//			printf("%02X", tmp[j]);
+//		}
+//		printf("\n");
 		CAddRoundKey<<<1,16*n>>>(cp, cW+((i-1) << 4));
-		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-		printf("%d add ",i);
-		for(j=0; j < 16*n; j++) {
-			printf("%02X", tmp[j]);
-		}
-		printf("\n");
+//		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//		printf("%d add ",i);
+//		for(j=0; j < 16*n; j++) {
+//			printf("%02X", tmp[j]);
+//		}
+//		printf("\n");
 		CInvMixColumns<<<1,16*n>>>(cp);
-		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
-		printf("%d mix ",i);
-		for(j=0; j < 16*n; j++) {
-			printf("%02X", tmp[j]);
-		}
-		printf("\n");
+//		cudaMemcpy(tmp, cp, sizeof(uint8_t)*16*n, cudaMemcpyDeviceToHost);
+//		printf("%d mix ",i);
+//		for(j=0; j < 16*n; j++) {
+//			printf("%02X", tmp[j]);
+//		}
+//		printf("\n");
 	}
 	CInvShiftRows<<<1,16*n>>>(cp);
 	CInvSubBytes<<<1,16*n>>>(cp);
@@ -271,23 +271,109 @@ void caes(uint8_t *cp, uint8_t *cW, uint8_t Nr, ulong n) {
 }
 
 void aes(uint8_t *tp, uint8_t *W, uint8_t Nr, ulong n) {
-	register uint8_t i,j;
-	for(j=1; j<=n; j++){
+
+	register uint8_t i;
+	ulong j;
+	for(j=0; j<n; j++){
+//		printf("-1 add ");
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
 		AddRoundKey(tp, W, j);
+//		printf("0 add ");
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
 		for(i=1; i<Nr; i++){
 			SubBytes(tp, j);
+//			printf("%d sub ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
 			ShiftRows(tp, j);
+//			printf("%d shi ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
 			MixColumns(tp, j);
+//			printf("%d mix ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
 			AddRoundKey(tp, W+(i << 4), j);
+//			printf("%d add ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
 		}
 		SubBytes(tp, j);
 		ShiftRows(tp, j);
 		AddRoundKey(tp, W+(i << 4), j);
+//		printf("%d add ",i);
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
 	}
 }
 
 void invAes(uint8_t *tp, uint8_t *W, uint8_t Nr, ulong n) {
-	//TODO
+
+	register uint8_t i;
+	ulong j;
+	for(j=0; j<n; j++){
+//		printf("-1 add ");
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
+		AddRoundKey(tp, W+(Nr << 4), j);
+//		printf("0 add ");
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
+		for(i=Nr; i>1; i++){
+			InvShiftRows(tp, j);
+//			printf("%d shi ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
+			InvSubBytes(tp, j);
+//			printf("%d sub ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
+			AddRoundKey(tp, W+((i-1) << 4), j);
+//			printf("%d add ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
+			InvMixColumns(tp, j);
+//			printf("%d mix ",i);
+//			for(k=0; k < 16*n; k++) {
+//				printf("%02X", tp[k]);
+//			}
+//			printf("\n");
+		}
+		InvShiftRows(tp, j);
+		InvSubBytes(tp, j);
+		AddRoundKey(tp, W, j);
+//		printf("%d add ",i);
+//		for(k=0; k < 16*n; k++) {
+//			printf("%02X", tp[k]);
+//		}
+//		printf("\n");
+	}
 }
 
 void ExpandKeys(uint8_t *key, uint8_t keysize, uint8_t *W, uint8_t Nk, uint8_t Nr) {
@@ -342,8 +428,8 @@ void aes_serial(uint8_t *in, uint8_t *chave, uint8_t **out, uint8_t tamanhoChave
 		invAes(in, W, Nr, offset);
 	}
 	*out = (uint8_t *)malloc(size*16*sizeof(uint8_t));
-	memcpy(*out, in, sizeof(in));
-	//printHexArray(out,(size/sizeof(uint8_t)));
+	memcpy(*out, in, size*16*sizeof(uint8_t));
+	//printHexArray(out,sizeof(out));
 	//for(register uint8_t i=0; i<(size/sizeof(uint8_t)); i++) {
 	//	printf("%d:", out[i]);
 	//}
@@ -447,11 +533,13 @@ int main(int argc, char **argv){
 	inicio = time(NULL);
 	printf("\ninicio %f\n", inicio);
 	printHexArray(in, tamanhoIn);
-//	aes_cuda(in, chave, &out, tamanhoChave << 3, offset, 1);
-//      printHexArray(out, tamanhoIn);
-//      aes_cuda(out, chave, &in, tamanhoChave << 3, offset, 0);
-//      printHexArray(in, tamanhoIn);
+	aes_cuda(in, chave, &out, tamanhoChave << 3, offset, 1);
+        printHexArray(out, tamanhoIn);
+	aes_cuda(out, chave, &out, tamanhoChave << 3, offset, 0);
+	printHexArray(in, tamanhoIn);
 	aes_serial(in, chave, &out, tamanhoChave << 3, offset, 1);
+	printHexArray(out, tamanhoIn);
+	aes_serial(out, chave, &out, tamanhoChave << 3, offset, 0);
 	printHexArray(out, tamanhoIn);
 	fim = time(NULL);
 	printf("\n Tempo gasto: %f\n", difftime(fim, inicio));
